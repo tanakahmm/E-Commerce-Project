68b8962d63865e554c196a56fcff0164d4707970
suraj261203 <sripadisuraj26@gmail.com>
2024-07-17 23:11:39 +0530

    second

--- e-commerce-project/controllers/authController.js
+++ e-commerce-project/controllers/authController.js
@@ -1,5 +1,6 @@
-import { hashPassword } from "../helpers/authHelper.js";
+import { comparePassword, hashPassword } from "../helpers/authHelper.js";
 import userModule from "../modules/userModule.js";
+import JWT from "jsonwebtoken";
 export const registerController=async(req,res)=>{
     try {
         const {name,email,password,phone,address}=req.body;
@@ -50,3 +51,55 @@ export const registerController=async(req,res)=>{
     }
 };
 
+export const loginController=async(req,res)=>{
+    try {
+        const{email,password} = req.body;
+        if(!email||!password){
+            return res.status(404).send({
+                success: false,
+                message: 'Invalid email or password'
+            })
+        }
+        const user = await userModule.findOne({email})
+        if(!user){
+            return res. status(404).send({
+                success: false,
+                message: 'Email is not registered'
+            })
+        }
+        const match = await comparePassword(password, user.password)
+        if(!match)
+        {
+            return res.status(200).send({
+                success:false,
+                message:'Invalid password'
+            });
+        }
+        const token=await JWT.sign({ _id: user._id }, process.env.JWT_SECRET, {
+            expiresIn: "7d",
+        });
+        res.status(200).send({
+            success:true,
+            message:'login sucessfully',
+            user:{
+                name:user.name,
+                email:user.email,
+                phone:user.phone,
+                address:user.address,
+            },
+            token,
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({
+            success:false,
+            message:'Error in login',
+            error
+        })
+    }
+}; 
+
+
+export const testController=(req,res)=>{
+    res.send('Protected Route');
+}
--- e-commerce-project/routes/authRoute.js
+++ e-commerce-project/routes/authRoute.js
@@ -1,7 +1,11 @@
 import express from 'express';
-import {registerController} from "../controllers/authController.js";
+import {registerController,loginController,testController} from "../controllers/authController.js";
+import { isAdmin, requireSignIn } from '../middleware/authMiddleware.js';
 const router=express.Router()
 
 router.post('/register',registerController)
 
+router.post('/login',loginController)
+
+router.get('/test',requireSignIn,isAdmin, testController);
 export default router
